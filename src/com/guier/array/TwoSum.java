81e50ddd82f0c2ecc9e981ce2cc2d720b5060265
Guiee <466007639@qq.com>
2020-09-09 08:44:23 +0800

    feat(Array): 1. Two Sum

--- src/com/guier/array/TwoSum.java
+++ src/com/guier/array/TwoSum.java
@@ -1,7 +1,26 @@
 package com.guier.array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * 1. Two Sum
  */
 public class TwoSum {
+    public static void main(String[] args) {
+        int target = 33;
+        int[] nums = new int[]{1, 3, 4, 11, 23, 22, 33, 77, 12};
+        System.out.println(Arrays.toString(new TwoSum().twoSum(nums, target)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
 }
